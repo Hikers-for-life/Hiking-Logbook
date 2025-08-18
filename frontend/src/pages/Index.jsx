4233e4d25f76b8825d60e8348881eeee49f55ace
Naledi Mogomotsi <naledimogomotsi3@gmail.com>
2025-08-18 10:04:53 +0200

    Update: added pages

---
+++ frontend/src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import { Navigation } from "../components/ui/navigation";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navigation />
+      { /* We add the rest when we have the full home page */ }
+    </div>
+  );
+};
+
+export default Index;
