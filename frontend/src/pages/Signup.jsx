d705f2250346c3d85352c6e9ed83bd8d6c1ed319
Naledi Mogomotsi <naledimogomotsi3@gmail.com>
2025-08-29 09:36:46 +0200

    chore: remove unnecessary imports

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import mountain from '../components/assets/forest-waterfall.jpg';
 import { ArrowLeft } from 'lucide-react';
 import { useAuth } from '../contexts/AuthContext.jsx';
